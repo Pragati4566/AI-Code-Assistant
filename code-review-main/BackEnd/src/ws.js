7c2ec5626fc5f65d36bebe9fffb196c93e8710ee
Pragati Chaudhary <savita845822@gmail.com>
2026-01-31 18:30:47 +0530

    Update ws.js

--- code-review-main/BackEnd/src/ws.js
+++ code-review-main/BackEnd/src/ws.js
@@ -4,10 +4,10 @@ import WebSocket from "ws"
 const wss = new WebSocket.Server({ port: 8080 })
 
 wss.on("connection", (ws) => {
-  console.log("Client connected")
+  console.log("Client connected") //client is establishing ocnnection //when client connected then msg will be send
 
   ws.on("message", (message) => {
-    console.log("Received:", message.toString())
+    console.log("Received:", message.toString())  //here msg will be send
 
     // demo response (later AI yahin lagega)
     ws.send("Server received your message")
