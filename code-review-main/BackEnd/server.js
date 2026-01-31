a2cd2f7bb5ba3fc4c9d72783bdb05b2a0a492788
Pragati Chaudhary <savita845822@gmail.com>
2026-01-31 18:45:24 +0530

    Update server.js

--- code-review-main/BackEnd/server.js
+++ code-review-main/BackEnd/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
+const http = require('http')
 const app = require('./src/app')
+const initWebSocket = require('./src/websocket')
 
+const server = http.createServer(app) // server connected to app(where express and rest apis will work)
 
+initWebSocket(server) // websocket get connected to server
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000') //server will run on this port (pehle here app.listen because server diretly app ko handover kar deta tha but now we have websocket here also)
 })
